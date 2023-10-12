02c5971c1858d8e63b6a277161c07d830dc99e56
LuckyAm20 <aptukovm@yandex.ru>
2023-10-12 03:17:14 +0300

    fix: Разбить код парсера на более мелкие функции

--- lab1/parser.cpp
+++ lab1/parser.cpp
@@ -1,5 +1,37 @@
 #include "parser.h"
 
+
+void Parser::push_pop(std::vector<std::string>& rpn, std::stack<std::string>& operators)
+{
+    rpn.push_back(operators.top());
+    operators.pop();
+}
+
+void Parser::parse_func(const std::string& expression, int& i, std::stack<std::string>& operators)
+{
+    std::stringstream ss;
+    while (i < expression.length() && isalpha(expression[i]))
+    {
+        ss << expression[i];
+        i++;
+    }
+    std::string function_name = ss.str();
+    operators.push(function_name);
+    i--;
+}
+
+void Parser::parse_digit(const std::string& expression, int& i, std::vector<std::string>& rpn)
+{
+    std::stringstream ss;
+    while (i < expression.length() && (isdigit(expression[i]) || expression[i] == '.'))
+    {
+        ss << expression[i];
+        i++;
+    }
+    rpn.push_back(ss.str());
+    i--;
+}
+
 std::vector<std::string> Parser::parse(const std::string& expression)
 {
     std::vector<std::string> rpn;
@@ -7,67 +39,40 @@ std::vector<std::string> Parser::parse(const std::string& expression)
 
     for (int i = 0; i < expression.length(); i++)
     {
-        if (isdigit(expression[i]) || expression[i] == '.') 
+        if (isdigit(expression[i]) || expression[i] == '.')
         {
-            std::stringstream ss;
-            while (i < expression.length() && (isdigit(expression[i]) || expression[i] == '.')) 
-            {
-                ss << expression[i];
-                i++;
-            }
-            rpn.push_back(ss.str());
-            i--;
+            parse_digit(expression, i, rpn);
         }
-        else if (expression[i] == '(') 
+        else if (expression[i] == '(')
             operators.push("(");
-        else if (expression[i] == ')') 
+        else if (expression[i] == ')')
         {
             while (operators.top() != "(")
-            {
-                rpn.push_back(operators.top());
-                operators.pop();
-            }
+                push_pop(rpn, operators);
             operators.pop();
             if (!operators.empty() && isalpha(operators.top()[0]))
-            {
-                rpn.push_back(operators.top());
-                operators.pop();
-            }
+                push_pop(rpn, operators);
+
         }
-        else if (operator_priority.find(expression[i]) != operator_priority.end()) 
+        else if (operator_priority.find(expression[i]) != operator_priority.end())
         {
-            if (expression[i] == '-' && (i == 0 || expression[i - 1] == '(' || operator_priority.find(expression[i-1]) != operator_priority.end()))
+            if (expression[i] == '-' && (i == 0 || expression[i - 1] == '(' || operator_priority.find(expression[i - 1]) != operator_priority.end()))
                 operators.push("u");
-            else 
+            else
             {
-                while (!operators.empty() && operator_priority[operators.top()[0]] >= operator_priority[expression[i]]) 
-                {
-                    rpn.push_back(operators.top());
-                    operators.pop();
-                }
+                while (!operators.empty() && operator_priority[operators.top()[0]] >= operator_priority[expression[i]])
+                    push_pop(rpn, operators);
                 operators.push(std::string(1, expression[i]));
             }
         }
-        else if (isalpha(expression[i])) 
-        {
-            std::stringstream ss;
-            while (i < expression.length() && isalpha(expression[i])) 
-            {
-                ss << expression[i];
-                i++;
-            }
-            std::string function_name = ss.str();
-            operators.push(function_name);
-            i--; 
-        }
+        else if (isalpha(expression[i]))
+            parse_func(expression, i, operators);
+        
     }
 
-    while (!operators.empty()) 
-    {
-        rpn.push_back(operators.top());
-        operators.pop();
-    }
-
+    while (!operators.empty())
+        push_pop(rpn, operators);
+    
     return rpn;
 }
 
--- lab1/parser.h
+++ lab1/parser.h
@@ -11,6 +11,9 @@ class Parser {
 public:
     Parser(Calc_Inf& calc_inf_) { operator_priority = calc_inf_.set_op_priority(); };
     std::vector<std::string> parse(const std::string& expression);
+    void push_pop(std::vector<std::string>& rpn, std::stack<std::string>& operators);
+    void parse_func(const std::string& expression, int& i, std::stack<std::string>& operators);
+    void parse_digit(const std::string& expression, int& i, std::vector<std::string>& rpn);
 
 private:
     std::map<char, int> operator_priority;
